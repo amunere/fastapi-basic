ef07157bcfe3998517fec367eacdeef4bbda0af3
amunere <amunere@gmail.com>
2024-03-25 20:04:02 +0500

    update app route

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from .config import settings
-from .routes import get_apps_router, TimedRoute
+from .routes import get_apps_router
 
 
 def get_application():
@@ -26,7 +26,7 @@ app = get_application()
 async def root():
     return {"message": "main"}
 
-app.include_router(get_apps_router()) #, prefix="/api/v1"
+app.include_router(get_apps_router(prefix="/api/v1")) 
 
 if __name__ == '__main__':
     uvicorn.run('main:app', reload=True)
